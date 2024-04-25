ae90ffe753fb961af51c85bbb2ac520d9e258654
piyushrai7271 <piyushrai7271@gmail.com>
2024-04-25 11:12:43 +0530

    problem in all over the code

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {registerUser} from "../controllers/user.controler.js";
-
-const router = Router();
-
-router.route("/register").post(registerUser)
-
-
-export default router;
