75b3116fc25f6c9118abd97e204ff7ae73ef24ec
piyushrai7271 <piyushrai7271@gmail.com>
2024-05-19 11:57:53 +0530

    setting router for endpoint

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,31 +1,52 @@
 import { Router } from "express";
-import {upload} from "../middleware/multer.middleware.js";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/register.controller.js";
+import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  getCurrentUser,
+  changeCurrentPassword,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/register.controller.js";
 
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser
-);
+// all the Routers from here ......
 
+
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 router.route("/login").post(loginUser);
 
-// secure router
+// secure routers ......
 
-router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-Token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
+router.route("/coverImage").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage);
+router.route("/c/:userName").get(verifyJWT,getUserChannelProfile);
+router.route("/history").get(verifyJWT,getWatchHistory);
+
 
 
 export default router;
